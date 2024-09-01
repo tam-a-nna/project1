7a95655ea591b40f277ffcdd05687751cfa41a62
tam-a-nna <fahmidachowdhury207@gmail.com>
2024-09-02 01:50:32 +0600

    Update DeckOfCardTest.java

--- mavenproject1/src/main/java/DeckOfCardTest.java
+++ mavenproject1/src/main/java/DeckOfCardTest.java
@@ -1,12 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author ASUS
- */
 public class DeckOfCardTest {
-    
+
+    public static void main(String[] args) {
+        DeckOfCards myDeckOfCards = new DeckOfCards();
+        myDeckOfCards.shuffle(); 
+
+        for (int i = 1; i <= 52; i++) 
+        {        
+            System.out.printf("%-19s", myDeckOfCards.dealCard());
+            if (i % 4 == 0) 
+            {
+                System.out.println();
+            }
+        }
+    }
+
 }
