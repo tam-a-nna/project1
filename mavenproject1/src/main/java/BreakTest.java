f797fc9310ebd193d953ce9a1437c5c1031caeca
tam-a-nna <fahmidachowdhury207@gmail.com>
2024-09-02 01:47:09 +0600

    Update BreakTest.java

--- mavenproject1/src/main/java/BreakTest.java
+++ mavenproject1/src/main/java/BreakTest.java
@@ -1,12 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author ASUS
- */
 public class BreakTest {
+    public static void main(String[] args) {
+        
+        int count;
+        for(count=1; count<=10; count++)
+        {
+            if(count == 5)
+                break;
+            System.out.println(count);
+        }
+        
+        System.out.println("Broke out of loop at count = "+count);
+    }
     
 }
