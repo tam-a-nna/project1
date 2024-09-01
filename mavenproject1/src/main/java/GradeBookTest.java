aa941d7f7808b0c98729f17881d3c17e76d7851c
tam-a-nna <fahmidachowdhury207@gmail.com>
2024-09-02 01:51:35 +0600

    Update GradeBookTest.java

--- mavenproject1/src/main/java/GradeBookTest.java
+++ mavenproject1/src/main/java/GradeBookTest.java
@@ -1,12 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author ASUS
- */
 public class GradeBookTest {
+    
+    public static void main(String[] args) {
+        
+        int[] gradesArray = {87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+        GradeBook myGradeBook = new GradeBook("CS101 Introduction to Java Programming", gradesArray);
+        
+        System.out.println("Welcome to the gradebook for "+myGradeBook.getCourseName());
+        myGradeBook.processGrades();
+        
+    }
     
 }
