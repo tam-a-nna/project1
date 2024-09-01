47c0d1e47b0c3307c311cfaffe12bf67ae76c894
tam-a-nna <fahmidachowdhury207@gmail.com>
2024-09-01 23:37:37 +0600

    Update GradeBook2.java

--- mavenproject1/src/main/java/GradeBook2.java
+++ mavenproject1/src/main/java/GradeBook2.java
@@ -1,12 +1,102 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author ASUS
- */
 public class GradeBook2 {
-    
+
+    private String courseName;
+    private int[][] grades;
+
+    public GradeBook2(String courseName, int[][] grades) {
+        this.courseName = courseName;
+        this.grades = grades;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void processGrades() {
+        outputGrades();
+
+        System.out.println("Lowest Grade = " + getMinimum() + "\nHighest Grade = " + getMaximum());
+        outputBarChart();
+    }
+
+    public int getMinimum() {
+        int lowGrade = grades[0][0];
+
+        for (int[] studentGrades : grades) {
+            for (int grade : studentGrades) {
+                if (grade < lowGrade) {
+                    lowGrade = grade;
+                }
+            }
+        }
+        return lowGrade;
+    }
+
+    public int getMaximum() {
+        int highGrade = grades[0][0];
+        for (int[] studentGrade : grades) {
+            for (int grade : studentGrade) {
+                if (grade > highGrade) {
+                    highGrade = grade;
+                }
+            }
+        }
+        return highGrade;
+    }
+
+    public double getAverage(int[] setOfGrades) {
+        int total = 0;
+        for (int grade : setOfGrades) {
+            total += grade;
+        }
+        return (double) total / setOfGrades.length;
+    }
+
+    public void outputBarChart() {
+        System.out.println("\nGrade distribution:");
+        int[] frequency = new int[11];
+        for (int[] studentGrades : grades) {
+            for (int grade : studentGrades) {
+                ++frequency[grade / 10];
+            }
+        }
+
+        for (int count = 0; count < frequency.length; count++) {
+            if (count == 10) {
+                System.out.printf("%5d: ", 100);
+            } else {
+                System.out.printf("%02d-%02d: ",
+                        count * 10, count * 10 + 9);
+            }
+            for (int stars = 0; stars < frequency[count]; stars++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public void outputGrades() {
+        System.out.println("\nThe grades are: ");
+        System.out.print("           ");
+
+        for (int test = 0; test < grades[0].length; test++) {
+            System.out.printf("Test %d ", test + 1);
+        }
+
+        System.out.println("Average");
+        for (int student = 0; student < grades.length; student++) {
+            System.out.printf("Student %2d", student + 1);
+
+            for (int test : grades[student]) {
+                System.out.printf("%8d", test);
+            }
+            double average = getAverage(grades[student]);
+            System.out.printf("%9.2f%n", average);
+        }
+    }
 }
