c39ef1a3fa26ea036e34751d0c3d424447d95108
tam-a-nna <fahmidachowdhury207@gmail.com>
2024-09-02 01:53:10 +0600

    Update MaximumFinder.java

--- mavenproject1/src/main/java/MaximumFinder.java
+++ mavenproject1/src/main/java/MaximumFinder.java
@@ -1,12 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.Scanner;
 
-/**
- *
- * @author ASUS
- */
 public class MaximumFinder {
-    
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter three floating-point values : ");
+        double number1 = input.nextDouble();
+        double number2 = input.nextDouble();
+        double number3 = input.nextDouble();
+
+        double result = maximum(number1, number2, number3);
+        System.out.println("Maximum is : "+result);
+    }
+
+    public static double maximum(double x, double y, double z) {
+        double maximumValue = x; 
+
+        if (y > maximumValue) {
+            maximumValue = y;
+        }
+        if (z > maximumValue) {
+            maximumValue = z;
+        }
+
+        return maximumValue;
+    }
+
 }
