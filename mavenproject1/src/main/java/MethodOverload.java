a02d1ba60de50093fb5d3d8a6bfcd3b3811bd69f
tam-a-nna <fahmidachowdhury207@gmail.com>
2024-09-02 01:53:42 +0600

    Update MethodOverload.java

--- mavenproject1/src/main/java/MethodOverload.java
+++ mavenproject1/src/main/java/MethodOverload.java
@@ -1,12 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author ASUS
- */
 public class MethodOverload {
+   
+    public static void main(String[] args) { 
+        System.out.printf("Square of integer 7 is %d%n", square(7));
+        System.out.printf("Square of double 7.5 is %f%n",square(7.5)); 
+    }
+    public static int square(int intValue)
+    {
+        return intValue*intValue;
+    }
+    public static double square(double doubleValue)
+    {
+        return doubleValue*doubleValue;
+    }
     
 }
