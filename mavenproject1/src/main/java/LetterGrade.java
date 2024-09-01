53617a064fc75f7b689a5a6bb57f5e379f202687
tam-a-nna <fahmidachowdhury207@gmail.com>
2024-09-02 01:52:10 +0600

    Update LetterGrade.java

--- mavenproject1/src/main/java/LetterGrade.java
+++ mavenproject1/src/main/java/LetterGrade.java
@@ -1,12 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.Scanner;
 
-/**
- *
- * @author ASUS
- */
 public class LetterGrade {
-    
+
+    public static void main(String[] args) {
+
+        int total = 0;
+        int gradeCounter = 0;
+        int aCount = 0;
+        int bCount = 0;
+        int cCount = 0;
+        int dCount = 0;
+        int fCount = 0;
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        while (n!=0) 
+        {
+            int grade = input.nextInt();
+            total += grade;
+            gradeCounter++;
+
+            switch(grade/10) 
+            {
+                case 9:  
+                case 10:  
+                    ++aCount;
+                    break; 
+                case 8:  
+                    ++bCount;
+                    break; 
+                case 7:  
+                    ++cCount;
+                    break; 
+                case 6: 
+                    ++dCount;
+                    break; 
+                default: 
+                    ++fCount;
+                    break; 
+            }
+            n--;
+        }
+        System.out.println("\nGrade Report: \n");
+        
+        if (gradeCounter != 0) 
+        {
+            double average = (double) total / gradeCounter; 
+            System.out.println("Total : "+total);
+            System.out.println("Average : "+average);
+            System.out.println("A : "+aCount+"\nB : "+bCount+
+                    "\nC : "+cCount+"\nD : "+dCount+"\nF : "+fCount);
+        }
+        else
+        {
+            System.out.println("No Grades Entered");
+        }
+    }
 }
