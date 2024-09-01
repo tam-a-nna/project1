cc141c5ceaa2856a4ffa96bd76ec30f146d5265b
tam-a-nna <fahmidachowdhury207@gmail.com>
2024-09-02 01:49:50 +0600

    Update Card.java

--- mavenproject1/src/main/java/Card.java
+++ mavenproject1/src/main/java/Card.java
@@ -1,12 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author ASUS
- */
 public class Card {
-    
+
+    private final String face;
+    private final String suit;
+
+    public Card(String cardFace, String cardSuit) 
+    {
+        this.face = cardFace;
+        this.suit = cardSuit;
+    }
+
+    public String toString() 
+    {
+        return face + " of " + suit;
+    }
+
 }
